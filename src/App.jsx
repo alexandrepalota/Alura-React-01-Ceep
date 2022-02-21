0702d5bdf37047b0e6292904b4afee2117733568
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2022-02-20 22:32:13 -0300

    Mais propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,22 @@ import './assets/App.css'
 import FormularioCadastro from "./components/FormularioCadastro";
 import ListaDeNotas from "./components/ListaDeNotas";
 class App extends Component {
+
+  constructor() {
+    super()
+    this.notas = []
+  }
+
+  criarNota(titulo, texto) {
+    const novaNota = {titulo, texto}
+    this.notas.push(novaNota)
+  }
+
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro />
-        <ListaDeNotas />
+        <FormularioCadastro onCriarNota={this.criarNota} />
+        <ListaDeNotas notas={this.notas} />
       </section>
     )
   }
