48a91d06d06cb38db78bb7537324c494beec48ec
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2022-02-21 12:33:20 -0300

    Importando SVG

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -1,14 +1,16 @@
 import { Component } from "react";
 import "./estilo.css"
+import {ReactComponent as DeleteSVG} from "../../assets/img/delete_black_24dp.svg" // precisa ser com letra maiúscula (DeleteSVG)
 
 class CardNota extends Component {
     render() {
         return (
             <section className="card-nota">
-                <header>
-                    <h3>{this.props.nota.titulo}</h3>
+                <header className="card-nota_cabecalho">
+                    <h3 className="card-nota_titulo">{this.props.nota.titulo}</h3>
+                    <DeleteSVG />
                 </header>
-                <p>{this.props.nota.texto}</p>
+                <p className="card-nota_texto">{this.props.nota.texto}</p>
             </section>
         );
     }
